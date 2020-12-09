5a8a665acfdbbb5f8209817519db13858ccc7a9a
Akshat <33050725+aaaakshat@users.noreply.github.com>
2020-12-09 10:23:18 +0800

    Add basic structure

---
+++ blockchain.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+class Blockchain(obj):
+    def __init__(self):
+        self.chain = []
+        self.curr_trnsactions = []
+
+    def new_block():
+        pass
+
+    def new_trnsaction():
+        pass
+
+    def hash(block):
+        pass
+
+    def last_block(self):
+        pass
+
+ 
+
+
+
